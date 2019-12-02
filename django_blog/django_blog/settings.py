0d4d0caa97373c6a4d0ceb2c36265662949bb152
ParkHyeonChae <park19996@naver.com>
2019-12-02 23:55:40 +0900

    user model 설정, superuser 생성

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users_management.UserManage'
+AUTH_USER_MODEL = 'user.User'
+
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
