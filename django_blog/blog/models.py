de08100b844856093031aad8f7fb7a6712ed639d
ParkHyeonChae <park19996@naver.com>
2019-12-07 04:09:45 +0900

    blog, helper modeling 후 migrate

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from helper.models import BaseModel
+from user.models import User
 
-# Create your models here.
+class Post(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, blank=False)
+    content = models.TextField()
+    image = models.ImageField(blank=True, null=True) # imageField 사용시 Plillow pip 설치
+
