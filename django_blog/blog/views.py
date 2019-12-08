24ae7914e94592aa0f76f5c28448fa8969a8a30a
ParkHyeonChae <park19996@naver.com>
2019-12-08 22:24:31 +0900

    post_list view, admin 연결

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from blog.models import Post
 
-# Create your views here.
+
+
+def posts_list(request):
+    posts = Post.objects.order_by('-created_at')
+
+    return render(request, 'blogs/posts_list.html',  context={'posts':posts})
